244e7cdc15254f928cbf3c3d1fbb1b38516e567f
Castar2107 <adam23icastarnado@inslaferreria.cat>
2025-09-29 15:39:16 +0200

    Menu

---
+++ menu.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles/main.css">
+    <title>Star Wars API</title>
+        <script type="text/javascript" src="scripts/main.js"></script>
+
+</head>
+<body>
+       <script type="text/javascript" src="scripts/main.js"></script>
+    <div class="title">
+        SWAPI (Grup 1)
+    </div>
+    <div class="text" id="authornames">
+        per Àlex Hernández, Joan López, Dani, Ivan
+    </div>
+    
+    <div class="button"><a href="people/">Personatges</a></div>
+    <div class="button"><a href="planets/">Planetes</a></div>
+    <div class="button"><a href="spaceships/">Naus</a></div>
+</body>
+
+</html>
